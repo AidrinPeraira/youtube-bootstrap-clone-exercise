d83dc792e65cb4148ab396529d5b95754a5c0879
Aidrin Peraira <aidrin.peraira@gmail.com>
2024-10-28 17:49:06 +0530

    finished the basic layout and responsiveness. Need to beautify

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+$('.sidebar').mCustomScrollbar();
+
+$('.chip-container').slick({
+    slidesToShow: 21,
+    slidesToScroll: 29,
+    arrows: true,
+    variableWidth: true,
+//    prevArrow: "<div><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;"><path d="M14.96 18.96 8 12l6.96-6.96.71.71L9.41 12l6.25 6.25-.7.71z"></path></svg></div>"
+    // Do the same for the next arrow
+});
+
+// what we did above was call the elemetn and gave the library funtion to do the jiba nd change the appearance as we wanted
